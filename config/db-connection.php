37d4feae706ad7751457d19670898800f08b19b4
Luc Mally <96879757+lucm9@users.noreply.github.com>
2024-03-16 18:43:02 -0500

    Update db-connection.php

--- config/db-connection.php
+++ config/db-connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $connection = mysqli_connect('localhost', 'root', '', 'votingdb');
+    $connection = mysqli_connect('172.31.37.187', 'admin', '', 'wordpressdb');
 } catch (Exception $e) {
     if (str_contains($e->getMessage(), 'Access denied')) { 
         die('Database connection failed');
